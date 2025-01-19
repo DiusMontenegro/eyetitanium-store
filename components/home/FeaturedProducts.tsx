f5518b50f7098fd07bc7f32f1b2fdb5cdc54a6b6
Dius Montenegro <montenegrodiussantos1@gmail.com>
2025-01-19 19:31:12 +0800

    feat: remove and replace the newly created wix-api functions to exixting featured product functions for cleaner code

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { delay } from "@/lib/utils";
-import { getWixClient } from "@/lib/wix-client.base";
 import Product from "../shared/Product";
+import { getCollectionBySlug } from "@/app/wix-api/collections";
+import { queryProducts } from "@/app/wix-api/products";
 
 async function FeaturedProducts() {
     await delay(1000);
 
-    const wixClient = getWixClient();
-    const { collection } = await wixClient.collections.getCollectionBySlug("featured-products");
+    // const wixClient = getWixClient();
+    // const { collection } = await wixClient.collections.getCollectionBySlug("featured-products");
+    const collection = await getCollectionBySlug("featured-products");
 
     if (!collection?._id) {
         return null;
     }
 
-    const featuredProducts = await wixClient.products
-        .queryProducts()
-        .hasSome("collectionIds", [collection._id])
-        .descending("lastUpdated")
-        .find();
+    // const featuredProducts = await wixClient.products
+    //     .queryProducts()
+    //     .hasSome("collectionIds", [collection._id])
+    //     .descending("lastUpdated")
+    //     .find();
+    const featuredProducts = await queryProducts({
+        collectionIds: collection._id,
+        sort: "last_updated",
+    });
 
     if (!featuredProducts.items.length) {
         return null;
